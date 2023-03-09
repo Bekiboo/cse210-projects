189ba5e708657fbbad5e1fcd6b197a193925bcc9
Bekiboo <julien.connault@gmail.com>
2023-03-09 19:19:08 +0300

    random scripture selected

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,25 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference ref1 = new Reference("John", 3, 16);
-        Scripture scripture1 = new Scripture(ref1, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+        List<Scripture> scriptures = new List<Scripture>();
 
-        Reference ref2 = new Reference("John", 3, 16, 17);
-        Scripture scripture2 = new Scripture(ref2, "For God did not send his Son into the world to condemn the world, but to save the world through him.");
+        scriptures.Add(new Scripture(new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."));
+        scriptures.Add(new Scripture(new Reference("1 Nephi", 3, 7), "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no bcommandments unto the children of men, save he shall cprepare a way for them that they may accomplish the thing which he commandeth them."));
+        scriptures.Add(new Scripture(new Reference("Romans", 8, 16, 17), "The Spirit itself beareth witness with our spirit, that we are the children of God: And if children, then heirs; heirs of God, and joint-heirs with Christ; if so be that we suffer with him, that we may be also glorified together."));
 
-        // Console.WriteLine(scripture1.GetScriptureString());
-        // Console.WriteLine(scripture2.GetScriptureString());
+        Random random = new Random();
+        int randomScripture = random.Next(0, scriptures.Count);
 
         string input = "";
 
-        while (input != "quit" && scripture1.GetRemainingWords() > 0)
+        while (input != "quit" && scriptures[randomScripture].GetRemainingWords() > 0)
         {
             Console.Clear();
-            Console.WriteLine(scripture1.GetScriptureString());
+            Console.WriteLine(scriptures[randomScripture].GetScriptureString());
             Console.WriteLine("");
             Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             input = Console.ReadLine();
-            scripture1.HideRandomWords();
+            scriptures[randomScripture].HideRandomWords();
         }
     }
 }
